93ae20695fc6bf0b4e53ffcd00910d526e839eed
kabelcher <kabelcher@wccnet.edu>
2022-10-03 19:36:12 -0400

    updates

---
+++ Projects/Project_2/project2_kabelcher_ex1.php
@@ -0,0 +1,33 @@
+<?php
+
+$list = 1;
+$sublist = 1;
+
+Class list {
+  for ($list=1; $list<=4; $list++){
+    echo "$list<br>";
+   }
+}
+
+Class sublist {
+  for ($sublist=1; $sublist<=5; $sublist++){
+    echo "$sublist<br>";
+   }
+}
+ ?>  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+<ul>
+ <li><?php echo list(); ?>
+    <ul>
+      <li><?php echo sublist(); ?></li>
+    </ul>
+ </li>
+ 
+</ul>
+</body>
+</html>
