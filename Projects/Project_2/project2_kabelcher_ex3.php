436497baf6f248d0703fc5f4f11dd96483109fcb
kabelcher <kabelcher@wccnet.edu>
2022-10-04 22:51:43 -0400

    updates

--- Projects/Project_2/project2_kabelcher_ex3.php
+++ Projects/Project_2/project2_kabelcher_ex3.php
@@ -1,30 +1,19 @@
 <?php
     $rows = 1;
     $columns = 1;
+    $table;
 
-    function buildTable(){
-        for ($rows = 1, $rows <= 15, $rows ++) {
-         return "Row $rows";
+    
+    while ($rows = 1, $rows <= 15, $rows ++) {
+        $table = <tr>;
+        while ($columns = 1, $columns <= 5, $columns ++) {
+            $table .= “<td>Row ($i), Cell ($j)</td>”;
         }
-        for ($columns = 1, $columns <= 5, $columns ++) {
-            return "Cell $columns";
-           }
     }
+       
 ?>
 <!DOCTYPE html>
 <html>
     <head></head>
-    <body>
-        <table border="1">
-            <tr>
-            <td>row 1 cell 1</td>
-            <td>row 1 cell 2</td>
-            <td>row 1 cell 3</td>
-            </tr>
-            <tr>
-            <td>row 2 cell 1</td>
-            <td>row 2 cell 2</td>
-            <td>row 2 cell 3</td>
-            </tr>
-    </body>
+    <body><?php echo $table; ?></body>
 </html>
